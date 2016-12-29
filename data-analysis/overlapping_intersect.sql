81cf48a3dfd2a986df03b021ff9ab0969d10571f
Phil <nospam@philipmallis.com>
2016-12-30 10:07:04 +1100

    Changed 'zipcode' to 'postcode'

--- data-analysis/overlapping_intersect.sql
+++ data-analysis/overlapping_intersect.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS mass_ave_segments_overlapping;
 SELECT *
 into mass_ave_segments_overlapping
 FROM (
-	SELECT a.cartodb_id, a.notes, a.name, a.zipcode, (ST_DUMP(ST_INTERSECTION(a.wkb_geometry, b.wkb_geometry))).geom as segment
+	SELECT a.cartodb_id, a.notes, a.name, a.postcode, (ST_DUMP(ST_INTERSECTION(a.wkb_geometry, b.wkb_geometry))).geom as segment
 	from mass_ave_test a
 	INNER JOIN mass_ave_test b ON ST_OVERLAPS(a.wkb_geometry,b.wkb_geometry)
 	WHERE a.cartodb_id <> b.cartodb_id
